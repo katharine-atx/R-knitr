413e7eb6abf8412bd57db5ed34e134eb51f96c30
katharine-atx <rovinsky.katharine@gmail.com>
2016-05-11 20:16:58 -0500

    Updated draft

--- knitrExample.R
+++ knitrExample.R
@@ -15,19 +15,44 @@
 #The dataset is stored in a comma-separated-value (CSV) file and there are a total of 17,568 observations in this dataset.
 
 # 1.Code for reading in the dataset and/or processing the data
-steps = read.csv("./repdata_data_activity/activity.csv", na.strings = "NA")
-str(steps)
+data = read.csv("./repdata_data_activity/activity.csv", na.strings = "NA")
+data$steps = as.numeric(data$steps)
 
 # 2.Histogram of the total number of steps taken each day
-
-st <- hist(as.numeric(steps$steps)), col = "blue", main = "Total Steps Per Day", xlab = "Steps"
+dailySteps = with(data, tapply(steps, date, sum, na.rm = TRUE))
+hist(dailySteps, breaks = 10, col = "blue", xlab = "steps per day", ylab = "frequency: days", main = "")
 
 # 3.Mean and median number of steps taken each day
+mean(dailySteps)
+median(dailySteps)
+
 # 4.Time series plot of the average number of steps taken
+plot(dailySteps, type = "l", ylab = "total steps", xlab = "daily counts: Oct - Nov 2012", axes = FALSE, col = "blue", lwd = 2)
+
 # 5.The 5-minute interval that, on average, contains the maximum number of steps
+meanInt = with(data, tapply(steps, interval, mean, na.rm = TRUE))
+meanInt[which(meanInt == max(meanInt))]
+
+# How many records are NA for step count?
+table(is.na(data$steps))
 # 6.Code to describe and show a strategy for imputing missing data
-table(is.na(steps$steps))
+# Prepare to impute NA values using mean for interval
+meanInt = data.frame(unique(data$interval),meanInt)
+colnames(meanInt) = c("interval", "avgsteps")
+meanInt$avgsteps = as.numeric(meanInt$avgsteps)
+# Merge mean interval data with original dataframe
+library(plyr)
+dataImpute = join(data, meanInt, by = "interval", type = "left", match = "all")
+
+# Add column for imputed values equal to steps, replacing NAs with interval means
+dataImpute$newSteps = dataImpute$steps
+stepsNA = is.na(dataImpute$newSteps)
+dataImpute$newSteps[stepsNA] = dataImpute$avgsteps[stepsNA]
+
 # 7.Histogram of the total number of steps taken each day after missing values are imputed
+newSteps = with(dataImpute, tapply(newSteps, date, sum, na.rm = TRUE))
+hist(newSteps, breaks = 10, col = "blue", xlab = "steps per day w/ imputed values", ylab = "frequency: days", main = "")
+
 # 8.Panel plot comparing the average number of steps taken per 5-minute interval across weekdays and weekends
-# 9.All of the R code needed to reproduce the results (numbers, plots, etc.) in the report
+
 
